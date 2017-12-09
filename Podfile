9021ee6bc1859e48a6ff1accfb0e1604006b4a61
cgoldsby <cgoldsby@gmail.com>
2017-12-09 09:28:21 -0500

    Add Snap.swift testing framework

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :tvos, '10.0'
+
+use_frameworks!
+
+target 'TvOSMoreButtonTests' do
+  pod 'Snap.swift', :git => 'https://github.com/skyweb07/Snap.swift'
+end
+
---
+++ Tests/Tests_StringTruncation.swift
@@ -0,0 +1,14 @@
+//
+//  Tests_StringTruncation.swift
+//  TvOSMoreButton
+//
+//  Created by Christopher Goldsby on 12/9/17.
+//  Copyright © 2017 Christopher Goldsby. All rights reserved.
+//
+
+import XCTest
+import Snap_swift
+
+final class Tests_StringTruncation: XCTestCase {
+
+}
